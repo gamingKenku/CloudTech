b119738dd946688e5364e5dbbca09953c5e35aa1
Artur <richt3r@mail.ru>
2024-03-07 16:22:48 +0500

    Добавьте файлы проекта.

---
+++ CloudTechLab1/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Net;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.IO;
+using System.Threading;
+
+namespace CloudTechLab1Server
+{
+    internal class EmployeeTCPServer
+    {
+        static TcpListener listener;
+
+        const int LIMIT = 5;
+
+        static Dictionary<string, string> employees = new Dictionary<string, string>()
+        {
+            {"john", "manager"},
+            {"jane", "steno"},
+            {"jim", "clerk"},
+            {"jack", "salesman"}
+        };
+
+
+        static void Main(string[] args)
+        {
+            listener = new TcpListener(IPAddress.Parse("127.0.0.1"), 13000);
+            listener.Start();
+
+#if LOG
+            Console.WriteLine("Server mounted, listening to port 13000.");
+#endif
+
+            for (int i = 0; i < LIMIT; i++) 
+            {
+                Thread t = new Thread(new ThreadStart(Service));
+                t.Start();
+            }
+        }
+
+        public static void Service()
+        {
+            while(true)
+            {
+                Socket soc = listener.AcceptSocket();
+#if LOG
+                Console.WriteLine("Connected: {0}", soc.RemoteEndPoint);
+#endif
+                try
+                {
+                    Stream s = new NetworkStream(soc);
+                    StreamReader sr = new StreamReader(s);
+                    StreamWriter sw = new StreamWriter(s);
+                    sw.AutoFlush = true;
+
+                    sw.WriteLine("{0} Employees available", employees.Count());
+
+                    while (true)
+                    {
+                        string name = sr.ReadLine();
+                        if (name == "" || name == null) break;
+                        string job = employees[name];
+                        if (job == null) job = "No such employee";
+                        sw.WriteLine(job);
+                    }
+                    s.Close();
+                }
+                catch (Exception e)
+                {
+#if LOG
+                    Console.WriteLine(e.Message);
+#endif
+                }
+#if LOG
+                    Console.WriteLine("Disconnected: {0}", soc.RemoteEndPoint);
+#endif
+                soc.Close();
+            }
+        }
+    }
+}
---
+++ CloudTechLab1Client/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+
+namespace CloudTechLab1Client
+{
+    class EmployeeTCPClient
+    {
+        public static void Main(string[] args)
+        {
+            //Порт нашего сервера
+            Int32 port = 12000;
+            string serverAddr = "127.0.0.1";
+            //Если адрес сервера передан как параметр
+            if (args.Length > 0)
+                serverAddr = args[0];
+
+            TcpClient client = new TcpClient(serverAddr, port);
+            try
+            {
+                Stream s = client.GetStream();
+                StreamReader sr = new StreamReader(s);
+                StreamWriter sw = new StreamWriter(s);
+                sw.AutoFlush = true;
+                Console.WriteLine(sr.ReadLine());
+                while (true)
+                {
+                    Console.Write("Name: ");
+                    string name = Console.ReadLine();
+                    sw.WriteLine(name);
+                    if (name == "") break;
+                    Console.WriteLine(sr.ReadLine());
+                }
+                s.Close();
+            }
+            finally
+            {
+                // code in finally block is guaranteed
+                // to execute irrespective of
+                // whether any exception occurs or does
+                // not occur in the try block
+                client.Close();
+            }
+        }
+    }
+}
